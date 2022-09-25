7cdcf3af841be3f3639dae01ef69bddf9567b76e
sagar.dobariya <sagar.dobariya@authbridge.com>
2022-09-25 20:06:48 +0530

    change simple end-point to secure one

--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -3,4 +3,4 @@ export const SERVER_BASE_URL_HTTP = "https://video-signalling.herokuapp.com";
 export const CREATE_ROOM = "/room";
 export const JOIN_ROOM = "/join";
 // export const WS_BASE_URL = process.env.SERVER_BASE_URL_WS;
-export const SERVER_BASE_URL_WS = "ws://video-signalling.herokuapp.com";
+export const SERVER_BASE_URL_WS = "wss://video-signalling.herokuapp.com";
